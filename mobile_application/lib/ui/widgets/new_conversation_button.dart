0beca17e0e0dab23178b75f647ab973b2a606ce9
Daiyaan Muhammad <daiyaan.muhammad2002@gmail.com>
2025-12-10 22:51:22 +0600

    New animations

--- mobile_application/lib/ui/widgets/new_conversation_button.dart
+++ mobile_application/lib/ui/widgets/new_conversation_button.dart
@@ -1,16 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
-class NewConversationButton extends StatelessWidget {
+class NewConversationButton extends StatefulWidget {
   final VoidCallback onPressed;
 
   const NewConversationButton({super.key, required this.onPressed});
 
   @override
+  State<NewConversationButton> createState() => _NewConversationButtonState();
+}
+
+class _NewConversationButtonState extends State<NewConversationButton>
+    with TickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return FloatingActionButton(
-      backgroundColor: Theme.of(context).colorScheme.primary,
-      onPressed: onPressed,
-      child: const Icon(Icons.add),
+    return TweenAnimationBuilder<double>(
+      tween: Tween<double>(begin: 0.0, end: 1.0),
+      duration: const Duration(milliseconds: 500),
+      builder: (context, value, child) {
+        return Transform.scale(
+          scale: value,
+          child: RotationTransition(
+            turns: Tween<double>(begin: -0.25, end: 0).animate(
+              CurvedAnimation(
+                parent: _controller,
+                curve: Curves.elasticOut,
+              ),
+            ),
+            child: FloatingActionButton(
+              backgroundColor: Theme.of(context).colorScheme.primary,
+              onPressed: widget.onPressed,
+              child: const Icon(Icons.add),
+            ),
+          ),
+        );
+      },
     );
   }
 }
